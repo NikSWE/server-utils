874fe8c69b5adda901394b1a1a84a2dab16e7c86
muj-programmer <nick.muj.programmer@gmail.com>
2020-02-05 16:14:49 +0530

    completed

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Server Utils
 
 This repository contains some utility scripts to setup server.
+
+## Install
+
+```sh
+$ sh -c "$(curl -sL https://github.com/muj-programmer/server-utils/raw/master/install-oh-my-zsh.sh)"
+```
--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -5,3 +5,16 @@ then
     echo "Error: zsh is not installed"
     exit 1
 fi
+
+ANTIGEN_DIR=$HOME/.antigen
+mkdir $ANTIGEN_DIR
+curl -sL git.io/antigen > $ANTIGEN_DIR/antigen.zsh
+echo "Installed: antigen in $ANTIGEN_DIR"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+mv $HOME/.zshrc $HOME/.zshrc-old
+curl -sL https://raw.githubusercontent.com/muj-programmer/server-utils/master/templates/zshrc > $HOME/.zshrc
+echo "Installed: oh-my-zsh in $HOME/.oh-my-zsh"
+
+echo "Action: changing default shell"
+chsh -s $(which zsh)
